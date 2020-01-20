470c5fbb9b26cffea7808ab5a677931c73246eba
Narciso Albarracin <nalbarracin@gmail.com>
2020-01-20 09:57:49 -0600

    Fix typos

--- src/HelloShapesMain.java
+++ src/HelloShapesMain.java
@@ -12,8 +12,8 @@ public class HelloShapesMain {
 		System.out.println("square1(name, size): " + square1.getName() + ", " + square1.getArea() );
 		System.out.println("triangle11(name, size): " + triangle1.getName() + ", " + triangle1.getArea() );
 		
-		Shape square2 = new Square("square1", 2.0);
-		Shape triangle2 = new Triangle("triangle1", 2.0, 2.0);		
+		Shape square2 = new Square("square2", 2.0);
+		Shape triangle2 = new Triangle("triangle2", 2.0, 2.0);		
 
 		System.out.println("square2(name, size): " + square2.getName() + ", " + square2.getArea() );
 		System.out.println("triangle2(name, size): " + triangle2.getName() + ", " + triangle2.getArea() );
